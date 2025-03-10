ac239637177f0f65a05586501c9538ffa5ec3aba
panicg <geeta.panicker@dieboldnixdorf.com>
2025-03-10 13:16:47 +0530

    7thMarch

---
+++ src/Lab001_JavaBasic/Lab002_HelloWorld.java
@@ -0,0 +1,12 @@
+package Lab001_JavaBasic;
+
+public class Lab002_HelloWorld {
+
+    public static void main(String[] args)
+    {
+        /*
+        This is code for comment
+         */
+        System.out.println("Hello World");
+    }
+}
